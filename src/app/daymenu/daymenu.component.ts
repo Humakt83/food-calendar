00e20ce55ae62e7938f9b0e41dde94cc0c396a67
Humakt83 <urlact@yahoo.com>
2017-12-17 06:09:35 +0200

    add other sections

--- src/app/daymenu/daymenu.component.ts
+++ src/app/daymenu/daymenu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../storage/storage.service';
-import { FoodCalendarDay } from '../foodcalendar/foodcalendarday';
+import { FoodCalendarDay, FoodMenuSection } from '../foodcalendar/foodcalendarday';
 import { FoodSection } from '../foodcalendar/foodsection';
 import { DateService } from '../week/date.service';
 
@@ -12,20 +12,46 @@ import { DateService } from '../week/date.service';
 export class DayMenuComponent {
 
     breakfasts: string[] = [];
+    lunches: string[] = [];
+    dinners: string[] = [];
+    snacks: string[] = [];
+
     private selectedDay: Date;
 
     constructor(private storage: StorageService, private dateService: DateService) {
         this.dateService.selectedDate.subscribe(result => {
+            const getFoodForSection = (section: FoodSection, sections: FoodMenuSection[]) => {
+                return sections.map(s => s.section).includes(section) ? 
+                    sections.filter(s => s.section === section)[0].food : [];
+            };
             this.selectedDay = result;
-            const foodDay = this.storage.getFood(result);
-            this.breakfasts = foodDay.sections.map(s => s.section).includes(FoodSection.BREAKFAST) ? 
-                foodDay.sections.filter(s => s.section === FoodSection.BREAKFAST)[0].food : [];
+            const foodSections = this.storage.getFood(result).sections;
+            this.breakfasts = getFoodForSection(FoodSection.BREAKFAST, foodSections);
+            this.lunches = getFoodForSection(FoodSection.LUNCH, foodSections);
+            this.dinners = getFoodForSection(FoodSection.DINNER, foodSections);     
+            this.snacks = getFoodForSection(FoodSection.SNACK, foodSections);
         });
     }
 
     dropBreakfast(event: DragEvent) {
+        this.dropFood(event, FoodSection.BREAKFAST, this.breakfasts);
+    }
+
+    dropLunch(event: DragEvent) {
+        this.dropFood(event, FoodSection.LUNCH, this.lunches);
+    }
+
+    dropDinner(event: DragEvent) {
+        this.dropFood(event, FoodSection.DINNER, this.dinners);
+    }
+
+    dropSnack(event: DragEvent) {
+        this.dropFood(event, FoodSection.SNACK, this.snacks);
+    }
+
+    private dropFood(event: DragEvent, section: FoodSection, arrayToAdd: string[]) {
         const food: string = event.dataTransfer.getData('text/plain');
-        this.breakfasts.push(food);
-        this.storage.storeFood(food, FoodSection.BREAKFAST, this.selectedDay);
+        arrayToAdd.push(food);
+        this.storage.storeFood(food, section, this.selectedDay);
     }
 }
